3b0bf93f2dc088d5a2a6b8ebf5f969e005be771a
yaxyobekuz <pubgn9642@gmail.com>
2026-02-26 00:59:50 +0500

    feat: add transaction type labels and debit transaction types

---
+++ src/features/transactions/data/transactionTypes.data.js
@@ -0,0 +1,9 @@
+export const transactionTypeLabels = {
+  daily: "Kunlik tanga",
+  weekly_school_bonus: "Haftalik maktab bonusi",
+  weekly_class_bonus: "Haftalik sinf bonusi",
+  market_purchase: "Do'kon xaridi",
+  market_refund: "Do'kon qaytimi",
+};
+
+export const debitTransactionTypes = ["market_purchase"];
