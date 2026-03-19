79e6a1984c77abf60992d2298658aafe40e9fe3a
yaxyobekuz <pubgn9642@gmail.com>
2026-03-19 10:34:01 +0500

    feat: refactor RootLayout to integrate subscription check and improve code organization

--- src/shared/layouts/RootLayout.jsx
+++ src/shared/layouts/RootLayout.jsx
@@ -4,24 +4,48 @@ import { Outlet } from "react-router-dom";
 // Tanstack Query
 import { useQuery } from "@tanstack/react-query";
 
-// API
-import { authAPI } from "@/features/auth/api/auth.api";
+// Hooks
+import useTelegram from "@/shared/hooks/useTelegram";
 
 // Components
 import BugReport from "../components/layout/BugReport";
 
-// Blocked page
+// API
+import { authAPI } from "@/features/auth/api/auth.api";
+import { socialNetworksAPI } from "@/features/social-networks/api/social-networks.api";
+
+// Blocked pages
 import BlockedPage from "@/features/penalties/pages/BlockedPage";
+import ChannelSubscriptionPage from "@/features/social-networks/pages/ChannelSubscriptionPage";
 
 const RootLayout = () => {
+  const { isTelegram, user: tgUser } = useTelegram();
+
   const { data: user } = useQuery({
     queryKey: ["auth", "me"],
     queryFn: () => authAPI.getMe().then((res) => res.data.data),
+  });
+
+  const { data: subscription, refetch: recheckSubscription } = useQuery({
+    queryKey: ["social-networks", "check-subscription", tgUser?.id],
+    queryFn: () =>
+      socialNetworksAPI
+        .checkSubscription(tgUser.id)
+        .then((res) => res.data.data),
     staleTime: 5 * 60 * 1000,
-    enabled: Boolean(localStorage.getItem("authToken")),
+    enabled: isTelegram && !!tgUser?.id && !!user,
   });
 
   if (user?.penaltyPoints >= 12) return <BlockedPage />;
+
+  if (subscription && !subscription?.subscribed) {
+    return (
+      <ChannelSubscriptionPage
+        onRecheck={recheckSubscription}
+        channels={subscription?.channels}
+      />
+    );
+  }
 
   return (
     <div className="bg-gray-100">
