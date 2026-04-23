9b1e931c7dcf15a309218ca6553c06312099cf8d
yaxyobekuz <pubgn9642@gmail.com>
2026-04-23 11:51:44 +0500

    feat: add premium and fine reduction transaction types to debitTransactionTypes

--- src/features/transactions/data/transactionTypes.data.js
+++ src/features/transactions/data/transactionTypes.data.js
@@ -6,6 +6,13 @@ export const transactionTypeLabels = {
   market_refund: "Do'kon qaytimi",
   manual_give: "Tanga berildi",
   manual_take: "Tanga olindi",
+  premium_purchase: "Premium obuna",
+  fine_reduction_purchase: "Jarima kamaytirishga sarflandi",
 };
 
-export const debitTransactionTypes = ["market_purchase", "manual_take"];
+export const debitTransactionTypes = [
+  "market_purchase",
+  "manual_take",
+  "premium_purchase",
+  "fine_reduction_purchase",
+];
